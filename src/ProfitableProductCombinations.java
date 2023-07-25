d2cc706ef1d215828774236bb44498da6c7731f5
Diego Vega <diegovega@me.com>
2023-07-25 12:22:15 +0200

    Add problem explanation

---
+++ src/ProfitableProductCombinations.java
@@ -0,0 +1,11 @@
+public class ProfitableProductCombinations {
+
+    /*
+    * Your startup has a large inventory of products with their respective prices stored in an integer array
+    * called ""products"". You want to identify all possible combinations of three products that can be sold
+    * together such that the total revenue generated is equal to $1000. Write a function that takes in the array
+    * of products and returns a list of all unique triplets satisfying the conditions mentioned above. Note that
+    * each product can only be used once in a triplet.
+    *
+    * */
+}
