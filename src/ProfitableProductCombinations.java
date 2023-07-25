b00a364af1b02843ceb4be98eab7826eb4b98bb8
Diego Vega <diegovega@me.com>
2023-07-25 13:08:14 +0200

    Add examples

--- src/ProfitableProductCombinations.java
+++ src/ProfitableProductCombinations.java
@@ -7,5 +7,17 @@ public class ProfitableProductCombinations {
     * of products and returns a list of all unique triplets satisfying the conditions mentioned above. Note that
     * each product can only be used once in a triplet.
     *
-    * */
+    * Example 1:
+    *   int[] products = {100, 200, 300, 400, 500, 600};
+    *   Output: [[100, 200, 700], [100, 300, 600], [100, 400, 500], [200, 300, 500]]
+    *
+    * Example 2:
+    *   int[] products = {50, 100, 150, 200, 250, 300, 350, 400, 450, 500};
+    *   Output: [[50, 200, 750], [50, 250, 700], [50, 300, 650], [50, 350, 600], [50, 400, 550],
+    *   [100, 150, 750], [100, 200, 700], [100, 250, 650], [100, 300, 600], [100, 350, 550],
+    *   [150, 200, 650], [150, 250, 600], [150, 300, 550], [200, 250, 550]]
+    *
+     * */
+
+
 }
