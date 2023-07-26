2b581180f3199a42066c1269d057bd1d23852d02
Diego Vega <diegovega@me.com>
2023-07-26 12:42:33 +0200

    Add problem explanation

---
+++ src/ZeroingOutMatrix.java
@@ -0,0 +1,12 @@
+public class ZeroingOutMatrix {
+
+    /*
+    * Our startup is building a data analytics dashboard that relies on a matrix to display data.
+    * Unfortunately, some of the data points are missing or have been corrupted and are represented by a 0 in
+    * the matrix. This creates a lot of confusion for our users and can lead to incorrect analysis. We need your
+    * help to implement a function that sets the entire row and column to 0's if a data point is missing or corrupted.
+    * Bonus: Do not use any extra memory
+    *
+    *
+    * */
+}
