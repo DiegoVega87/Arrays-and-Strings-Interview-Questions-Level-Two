1cc90997a39dbdf843e612f1a1edb76f8a9cce8a
Diego Vega <diegovega@me.com>
2023-07-26 12:44:42 +0200

    Add examples

--- src/ZeroingOutMatrix.java
+++ src/ZeroingOutMatrix.java
@@ -7,6 +7,14 @@ public class ZeroingOutMatrix {
     * help to implement a function that sets the entire row and column to 0's if a data point is missing or corrupted.
     * Bonus: Do not use any extra memory
     *
+    * Example 1:
+    *
+    *   matrix = {{0, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+    *   Output = {{0, 0, 0}, {4, 5, 0}, {0, 0, 0}}
+    *
+    * Example 2:
+    *   int[][] matrix = {{1, 2, 3}, {4, 0, 6}, {7, 8, 9}};
+    *   Output = {{1, 0, 3}, {0, 0, 0}, {7, 0, 9}}
     *
     * */
 }
