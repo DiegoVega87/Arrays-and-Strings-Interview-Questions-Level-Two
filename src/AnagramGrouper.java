8573f4b571073ef4f19e73dc6c3949f0ee2f5ee4
Diego Vega <diegovega@me.com>
2023-07-28 18:16:32 +0200

    Add examples

--- src/AnagramGrouper.java
+++ src/AnagramGrouper.java
@@ -5,5 +5,26 @@ public class AnagramGrouper {
     * We want to group all the anagrams together to help our users learn and remember vocabulary more efficiently.
     * We need you to write a function that takes in an array of strings returns a list of lists, where each list
     * contains strings that are anagrams of each other.
-     * */
+    *
+    *
+    * Example 1:
+    *   String[] words = {""eat"", ""tea"", ""tan"", ""ate"", ""nat"", ""bat""};
+    *   List<List<String>> expectedOutput = new ArrayList<>();
+    *   expectedOutput.add(Arrays.asList(""eat"", ""tea"", ""ate""));
+    *   expectedOutput.add(Arrays.asList(""tan"", ""nat""));
+    *   expectedOutput.add(Arrays.asList(""bat""));
+    *   List<List<String>> actualOutput = groupAnagrams(words);
+    *   assert expectedOutput.equals(actualOutput);
+    *
+    *
+    * Example 2:
+    *   String[] words = {""listen"", ""silent"", ""elbow"", ""below"", ""state"", ""taste""};
+    *   List<List<String>> expectedOutput = new ArrayList<>();
+    *   expectedOutput.add(Arrays.asList(""listen"", ""silent""));
+    *   expectedOutput.add(Arrays.asList(""elbow"", ""below""));
+    *   expectedOutput.add(Arrays.asList(""state"", ""taste""));
+    *   List<List<String>> actualOutput = groupAnagrams(words);
+    *   assert expectedOutput.equals(actualOutput);
+    *
+    * */
 }
