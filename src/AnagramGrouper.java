634d512c9ae5b1a5f7a3ea874fb7af75eb50d6f3
Diego Vega <diegovega@me.com>
2023-07-28 17:39:17 +0200

    Add problem explanation

---
+++ src/AnagramGrouper.java
@@ -0,0 +1,9 @@
+public class AnagramGrouper {
+
+    /*
+    * As a startup working on a language learning platform, we have a dataset of words in different languages.
+    * We want to group all the anagrams together to help our users learn and remember vocabulary more efficiently.
+    * We need you to write a function that takes in an array of strings returns a list of lists, where each list
+    * contains strings that are anagrams of each other.
+     * */
+}
