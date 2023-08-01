a5f112613bf96dacabec814e0312b23ec15c8426
Diego Vega <diegovega@me.com>
2023-08-01 16:07:45 +0200

    Add problem explanation

---
+++ src/LongestPalindrome.java
@@ -0,0 +1,10 @@
+public class LongestPalindrome {
+
+    /*
+    * We have a marketing campaign where we need to identify the longest palindromic substring from a given string.
+    * The palindromic substring can help us identify the words that can be used as catchy slogans or product names.
+    * Can you create a function that takes a string as input and returns the longest palindromic substring in it?
+    *
+    *
+    * */
+}
