a945da573f4813edb680d9551037c0c495640332
Diego Vega <diegovega@me.com>
2023-08-01 10:50:50 +0200

    Add problem explanation

---
+++ src/PasswordSecurity.java
@@ -0,0 +1,10 @@
+public class PasswordSecurity {
+
+    /*
+    * In order to provide the user with information about the password's strength, we aim to notify them about
+    * the longest substring within the password that does not contain any repeated characters. To achieve this,
+    * we require a function that takes a password string as input and returns the length of the longest substring
+    * without any repeated characters.
+    *
+    * */
+}
