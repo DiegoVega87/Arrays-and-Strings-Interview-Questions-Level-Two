d6bf02c0c1df336604175cd76fd2b9ae54debc4f
Diego Vega <diegovega@me.com>
2023-08-02 16:58:37 +0200

    Add problem explanation

---
+++ src/IdentifyingTrends.java
@@ -0,0 +1,12 @@
+public class IdentifyingTrends {
+
+    /*
+    * Your startup is looking to invest in the stock market and you want to find a way to identify potential
+    * opportunities for profit. You have historical data of daily stock prices in an integer array called prices.
+    * Write a function that returns true if there exists a triplet of indices (i, j, k) such that i < j < k and
+    * prices[i] < prices[j] < prices[k]. This indicates that there is an increasing trend in the stock prices
+    * and investing in this sequence could lead to profit. If no such indices exists, return false.
+    *
+    *
+    * * */
+}
