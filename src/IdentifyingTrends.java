6c46a889fd793f5f7a491268106074f10a88238e
Diego Vega <diegovega@me.com>
2023-08-02 17:13:11 +0200

    Add examples

--- src/IdentifyingTrends.java
+++ src/IdentifyingTrends.java
@@ -7,6 +7,15 @@ public class IdentifyingTrends {
     * prices[i] < prices[j] < prices[k]. This indicates that there is an increasing trend in the stock prices
     * and investing in this sequence could lead to profit. If no such indices exists, return false.
     *
+    * Example 1:
+    *   Input: prices = {1, 2, 3, 4, 5}
+    *   Output: true
+    *   Explanation: There exists a triplet (i, j, k) such that i < j < k and prices[i] < prices[j] < prices[k].
+    *   For example, (0, 1, 2) or (1, 2, 3) or (2, 3, 4).
     *
+    * Example 2:
+    *   Input: prices = {5, 4, 3, 2, 1}
+    *   Output: false
+    *   Explanation: There is no triplet (i, j, k) such that i < j < k and prices[i] < prices[j] < prices[k].
     * * */
 }
